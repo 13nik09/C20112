349d122432856ea77a01a1554745951bf2f65410
13nik <13nik09@gmail.com>
2024-11-03 19:00:09 -0500

    Lesson 1-2

---
+++ Lesson2/main.py
@@ -0,0 +1,8 @@
+from student import Student
+
+chupryn = Student("Ivan", 15.0, True)
+kapitanskiy = Student("Roman", 15.4, True)
+
+print(f"Name: {chupryn.Name}\nAge: {chupryn.Age}\nGender: {chupryn.Gender}")
+print("   ")
+print(f"Name: {kapitanskiy.Name}\nAge: {kapitanskiy.Age}\nGender: {kapitanskiy.Gender}")
--- Lesson2/student.py
+++ Lesson2/student.py
@@ -1,1 +1,5 @@
-print("Hello world!")
+class Student:
+    def __init__(self, name: str = "Test", age: float = 0.0, gender: bool = False):
+        self.Name = name
+        self.Age = age
+        self.Gender = gender
