0b5df95b00e94f5890e997f41515072ad32d4689
13nik <13nik09@gmail.com>
2024-11-03 20:24:26 -0500

    Lesson 1-2

---
+++ Dz2/main.py
@@ -0,0 +1,39 @@
+class Dog:
+    def __init__(self, name):
+        self.name = name
+        self.energy = 50
+        self.hunger = 50
+        self.happiness = 0
+
+        print(f"{self.name} прокинувся. Голод: {self.hunger}, Енергія: {self.energy}, Щастя: {self.happiness}")
+        print("    ")
+
+    def eat(self):
+        self.hunger -= 50
+        self.energy += 20
+        self.happiness += 10
+        print(f"{self.name} їсть. Голод: {self.hunger}, Енергія: {self.energy}, Щастя: {self.happiness}")
+
+    def sleep(self):
+        self.energy += 30
+        self.hunger += 10
+        self.happiness += 20
+        print(f"{self.name} заснув. Голод: {self.hunger}, Енергія: {self.energy}, Щастя: {self.happiness}")
+
+    def eat2(self):
+        self.hunger -= 10
+        self.happiness += 10
+        print(f"{self.name} перекусує. Голод: {self.hunger}, Енергія: {self.energy}, Щастя: {self.happiness}")
+
+    def play(self):
+        self.happiness += 60
+        self.energy -= 20
+        self.hunger += 30
+        print(f"{self.name} грається. Голод: {self.hunger}, Енергія: {self.energy}, Щастя: {self.happiness}")
+
+barni = Dog("Барні")
+
+barni.eat()
+barni.sleep()
+barni.eat2()
+barni.play()
